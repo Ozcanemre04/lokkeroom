c3124105408b12dbeeb62f06c086b129b2bbb216
ozcanemre <ozcan.emre1608@gmail.com>
2022-06-17 10:42:44 +0200

    d

--- database.mjs
+++ database.mjs
@@ -3,7 +3,10 @@ const {Pool} = pkg;
 import dotenv from 'dotenv';
 dotenv.config()
  const pool = new Pool({
-   connectionString:process.env.DB_CONNECTION_STRING 
+   connectionString:process.env.DATABASE_URL,
+   ssl: {
+    rejectUnauthorized: false
+  }
 });
 
 export default pool
